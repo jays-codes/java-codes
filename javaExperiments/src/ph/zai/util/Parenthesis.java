9df0d296a6f34ecd9f738cc6b9dd49acca19e36b
jay-menorca <zaimenorca@gmail.com>
2014-02-20 19:53:01 +0800

    Added Parenthesis class that checks pairing of parenthesis - tested

---
+++ javaExperiments/src/ph/zai/util/Parenthesis.java
@@ -0,0 +1,28 @@
+package ph.zai.util;
+
+import java.util.Stack;
+
+public class Parenthesis {
+
+	public static boolean isValidSet(String xStr) {
+
+		char[] chars = xStr.toCharArray();
+		Stack<Character> charStack = new Stack<Character>();
+		
+		for (int i=0;i<chars.length;i++){
+			char c = chars[i];
+			if (c=='(' || c=='[' || c=='{') charStack.push(c);
+			else {
+				if (
+						charStack.isEmpty() ||
+						(c==')' && charStack.pop()!='(') ||
+						(c==']' && charStack.pop()!='[') ||
+						(c=='}' && charStack.pop()!='{') 
+				)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+}
