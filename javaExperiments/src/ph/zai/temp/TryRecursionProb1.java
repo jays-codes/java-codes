9d6169cd880c9e99f7225878d7f45de0165c05dc
jay-menorca <zaimenorca@gmail.com>
2014-02-25 22:01:34 +0800

    adding answers to recursion exercises - sedgewick algo 4e section 1.1.18

---
+++ javaExperiments/src/ph/zai/temp/TryRecursionProb1.java
@@ -0,0 +1,20 @@
+package ph.zai.temp;
+
+public class TryRecursionProb1 {
+
+	public static void main(String[] args) {
+		String ans = TryRecursionProb1.exR1(6);
+		System.out.println("answer=" + ans);
+		
+		int modu = 1 % 2;
+		int x=0;
+	}
+
+	public static String exR1(int n) {
+		String retStr="";
+		if (n <= 0)
+			return "";
+		retStr = exR1(n - 3) + n + exR1(n - 2) + n;
+		return retStr;
+	}
+}
---
+++ javaExperiments/src/ph/zai/temp/TryRecursiveProblem2.java
@@ -0,0 +1,33 @@
+package ph.zai.temp;
+
+public class TryRecursiveProblem2 {
+
+	public static void main(String[] args) {
+		int val = TryRecursiveProblem2.mystery(25, 2);
+		System.out.println(val);
+		
+		val = TryRecursiveProblem2.mystery(3, 11);
+		System.out.println(val);
+
+		val = TryRecursiveProblem2.mystery2(2, 25);
+		System.out.println(val);
+
+
+	}
+
+	public static int mystery(int a, int b) {
+		if (b == 0)
+			return 0;
+		if (b % 2 == 0)
+			return mystery(a + a, b / 2);
+		return mystery(a + a, b / 2) + a;
+	}
+	
+	public static int mystery2(int a, int b) {
+		if (b == 0)
+			return 1;
+		if (b % 2 == 0)
+			return mystery2(a * a, b / 2);
+		return mystery2(a * a, b / 2) * a;
+	}
+}
