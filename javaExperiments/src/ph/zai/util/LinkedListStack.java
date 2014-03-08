2d64fdc6a29df1a644f12f74c695e1604ef2fbc8
jay-menorca <zaimenorca@gmail.com>
2014-03-08 10:43:44 +0800

    added LinkedList implementation of a stack

---
+++ javaExperiments/src/ph/zai/util/LinkedListStack.java
@@ -0,0 +1,67 @@
+package ph.zai.util;
+
+import java.util.Iterator;
+
+public class LinkedListStack<Item> {
+	private Node first = null;
+	private int nodeCount=0;
+	
+	private class Node{
+		Item item;
+		Node next;
+	}
+	
+	public void push(Item item){
+		Node oldFirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldFirst;
+		nodeCount++;
+	}
+	
+	public Item pop(){
+		Item poppedItem =  first.item;
+		first = first.next;
+		nodeCount--;
+		return poppedItem;
+	}
+	
+	public boolean isEmpty(){return first==null;}
+	public int size(){return nodeCount;}
+	
+	
+	public Iterator<Node> iterator() {
+		return new ReverseArrayIterator();
+	}
+
+	private class ReverseArrayIterator implements Iterator<Node> { // Support
+
+		int i = nodeCount;
+		public boolean hasNext() {
+			return i > 0;
+		}
+
+		public Node next() {
+			return first.next;
+		}
+
+		public void remove() {
+			i--;
+		}
+	}
+
+	public String toString() {
+		Node node = first;
+		
+		String str = "\n" + first.item + "\n";
+		
+		while (!(node.next==null)){
+			node = node.next;
+			str = str + node.item + "\n";
+		}
+		return "stack contains " + size() + " item(s) : "
+				+ str;
+		
+		
+	}	
+}
--- javaExperiments/src/ph/zai/util/impl/StackADTTester.java
+++ javaExperiments/src/ph/zai/util/impl/StackADTTester.java
@@ -1,12 +1,12 @@
 package ph.zai.util.impl;
 
-import ph.zai.util.ResizableArrayStack;
+import ph.zai.util.LinkedListStack;
 
-public class FixedCapacityStackTest {
+public class StackADTTester {
 
 	public static void main(String[] args) {
 
-		ResizableArrayStack<String> fcs = new ResizableArrayStack<String>();
+		LinkedListStack<String> fcs = new LinkedListStack<String>();
 		System.out.println("stack empty? : " + fcs.isEmpty());
 		
 		fcs.push("apple");
