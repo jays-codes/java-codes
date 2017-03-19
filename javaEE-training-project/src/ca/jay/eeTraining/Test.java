8cf9c1b0959d3a4e60bc8c0705ff56a872ff8b37
Jay Menorca <zaimenorca@gmail.com>
2017-03-19 18:04:50 -0400

    GIT test: create eclipse proj; upload to remote

---
+++ javaEE-training-project/src/ca/jay/eeTraining/Test.java
@@ -0,0 +1,10 @@
+package ca.jay.eeTraining;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Testing GIT workflows : creating an eclipse java project; upload to remote");
+
+	}
+
+}
--- javaEE-training-project/src/ca/jay/javaEE/Test.java
+++
@@ -1,9 +0,0 @@
-package ca.jay.javaEE;
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("HELLO WORLD!!! THIS IS JAY!!");
-	}
-
-}
